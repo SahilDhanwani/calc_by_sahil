d0ddd7fbc188e04c60a0394ba4491b44398eb609
SahilDhanwani <sahildhanwani291203@gmail.com>
2025-01-16 20:51:51 +0530

    Refactor color definitions and add Keypad and Button components with styling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { myColor } from "./src/styles/Color";
+import { Theme } from "./src/context/Theme";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Keypad from "./src/components/Keypad";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Theme.Provider value={theme}>
+      <SafeAreaView
+        style={[
+          styles.container,
+          theme === "light" ? {} : { backgroundColor: myColor.dark },
+        ]}
+      >
+        <View style={styles.switchContainer}>
+          <Icon
+            name={theme === "light" ? "weather-sunny" : "weather-night"}
+            size={30}
+            color={theme === "light" ? myColor.black : myColor.white}
+            onPress={() => setTheme(theme === "light" ? "dark" : "light")}
+          />
+        </View>
+
+        <Keypad theme={theme} />
+      </SafeAreaView>
+    </Theme.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColor.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 780,
+  },
+  switchContainer: {
+    position: "absolute",
+    top: 50,
+    left: 20,
+    zIndex: 1,
   },
 });
